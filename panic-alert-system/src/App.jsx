b5fb8e0d62e4e271e9f793bf9089f2436b04930d
zincandcement <clareanikwe@gmail.com>
2026-09-08 22:29:13 +0100

    fix

--- panic-alert-system/src/App.jsx
+++ panic-alert-system/src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from "react";
 import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/LandingPage'
-
+import LandingPage from './pages/LandingPage'
 
 function App() {
-    
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Replace this with actual login call (fetch/axios to your backend)
-    console.log({ username, password });
-  };
-
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/landing" element={<LandingPage />} />
-    </Routes> 
+    </Routes>
   )
-   
 }
 
 export default App;
--- panic-alert-system/src/pages/LandingPage.jsx
+++ panic-alert-system/src/pages/LandingPage.jsx
@@ -16,4 +16,4 @@ export default function LandingPage() {
       <Footer />
     </div>
   );
-}s
+}
